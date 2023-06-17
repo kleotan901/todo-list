bfeba2996f2a8f515c7e4c43b0a86e0dff25f992
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-17 22:05:54 +0200

    Added Tag & Task to models.py

--- task_list/models.py
+++ task_list/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_created=True)
+    deadline = models.DateTimeField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="tasks",
+        default=None,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.content
