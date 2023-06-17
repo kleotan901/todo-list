5ff3bb2d20fdc27450af6d98f1853fabdbd70ae8
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-17 23:43:07 +0200

    Added class HomePage to views.py and created templates base.html & index.html

--- task_list/views.py
+++ task_list/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task_list.models import Task, Tag
+
+
+class HomePage(generic.ListView):
+    template_name = "task_list/index.html"
+    queryset = Task.objects.all()
+    context_object_name = "task_list"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HomePage, self).get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        return context
+
