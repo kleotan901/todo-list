dff7f4c41712996008060f81017fad6d290999bc
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-18 00:17:45 +0200

    Added models to admin.py

--- task_list/admin.py
+++ task_list/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Task, Tag
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("content",)
+    list_filter = ("is_completed", "tags")
+    list_display = ("content", "datetime", "deadline")
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name", )
