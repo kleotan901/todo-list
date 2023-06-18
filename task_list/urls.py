71a94deadd6c45083096cf10ef1ce3ffd10d99ec
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-18 11:51:44 +0200

    Added paths to TagListView TagCreateView TagUpdateView & TagDeleteView to urls.py

--- task_list/urls.py
+++ task_list/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path, include
 
-from task_list.views import HomePage, TaskCreateView, TaskUpdateView, TaskDeleteView, ChangeTaskStatus
+from task_list.views import (
+    HomePage,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    ChangeTaskStatus,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
 
 urlpatterns = [
     path("", HomePage.as_view(), name="index"),
@@ -8,6 +18,10 @@ urlpatterns = [
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tasks/<int:pk>/change-status/", ChangeTaskStatus.as_view(), name="task-change-status"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "task-list"
