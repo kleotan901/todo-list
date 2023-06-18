8ad58fa3f77033ecc3e5d1b1534063dd8159228a
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-18 10:12:41 +0200

    Added path to task_delete page in urls.py

--- task_list/urls.py
+++ task_list/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-from task_list.views import HomePage, TaskCreateView, TaskUpdateView
+from task_list.views import HomePage, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path("", HomePage.as_view(), name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "task-list"
