3f4c1d8b1e5ac717fb4c6ca04660ca3279deaf8b
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-18 09:53:34 +0200

    Added TaskForm to forms.py

---
+++ task_list/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from task_list.models import Tag, Task
+
+
+class TaskForm(forms.ModelForm):
+    datetime = forms.DateTimeField(
+        widget=forms.DateInput(attrs={"type": "date"})
+    )
+    deadline = forms.DateTimeField(
+        widget=forms.DateInput(attrs={"type": "date"})
+    )
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
