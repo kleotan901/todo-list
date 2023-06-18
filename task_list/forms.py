3e62502f20abda5f17eed5e0237a976b337f7a4d
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-18 11:52:14 +0200

    Added TagForm to forms.py

--- task_list/forms.py
+++ task_list/forms.py
@@ -18,3 +18,8 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = "__all__"
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = "__all__"
