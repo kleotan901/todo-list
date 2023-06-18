55229a6bc05de135ead9340aa609ccf409e0726a
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-18 11:10:13 +0200

    Updated urls.py with path to ChangeTaskStatus

--- task_list/urls.py
+++ task_list/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 
-from task_list.views import HomePage, TaskCreateView, TaskUpdateView, TaskDeleteView
+from task_list.views import HomePage, TaskCreateView, TaskUpdateView, TaskDeleteView, ChangeTaskStatus
 
 urlpatterns = [
     path("", HomePage.as_view(), name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/change-status/", ChangeTaskStatus.as_view(), name="task-change-status"),
 ]
 
 app_name = "task-list"
