d614e289b2b4c992fa9d2773783979c64c1c0112
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-18 10:22:30 +0200

    Added ordering to views.py

--- task_list/views.py
+++ task_list/views.py
@@ -9,6 +9,7 @@ class HomePage(generic.ListView):
     template_name = "task_list/index.html"
     queryset = Task.objects.all()
     context_object_name = "task_list"
+    ordering = ["is_completed"]
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomePage, self).get_context_data(**kwargs)
