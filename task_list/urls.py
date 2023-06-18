98df90f755c12fd74b44e4925380a01275684728
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-18 09:56:56 +0200

    Updated task form page paths in urls.py

--- task_list/urls.py
+++ task_list/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from task_list.views import HomePage
+from task_list.views import HomePage, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
     path("", HomePage.as_view(), name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
 
-app_name = "task_list"
+app_name = "task-list"
